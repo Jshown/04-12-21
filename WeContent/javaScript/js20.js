56c73921215009a6814b02cc8b0bd4d12956188c
acer <acer@10.0.0.11>
2022-07-16 11:51:12 +1000

    .

--- WeContent/javaScript/js20.js
+++ WeContent/javaScript/js20.js
@@ -26,56 +26,51 @@ number.forEach(function(item){
 	  if (isLeftNumber && isOperator && isRightNumber){
 			document.querySelector("#answer").innerHTML = eval(document.querySelector("#screen").innerHTML.replaceAll("×", "*").replaceAll("÷", "/"));
 		}
-//		if(isLeftNumber&isOperator)){
-//			
-//		}	else(isLeftNumber&&isOperator&&isRightNumber){
-//			isLeftNumber = true;
-//			isOperator = false;
-//			isRightNumber = false;
-//		}
 	});
 });
 
 // 2. Operations
-const other = document.querySelectorAll(".item");
-other.forEach(function(other){
+const other = document.querySelectorAll(".other");
+other.forEach(function(item){
 	item.addEventListener("click", function(){
 		let screenHTML = document.querySelector("#screen").innerHTML;
 		let last = document.querySelector("#screen").innerHTML.charAt(document.querySelector("#screen").innerHTML.length-1 );
-
-		if(screenHTML != null && screenHTML != ""){
-			isLeftNumber = false;
-			isOperator = false;
-			isRightNumber = false;
-		}else 	if ( isNaN ( Number (last) ) ){
-	//			document.querySelector ("#screen").innerHTML = document.querySelector("#screen").innerHTML.replace(last, item.innerHTML)4
-					document.querySelector("#screen").innerHTML = screenHTML.substr(0, screenHTML.length-1 ) + item.innerHTML;
-							} else {
-				document.querySelector("#screen").innerHTML += item.innerHTML;
-			}
 			
 		isLeftNumber   = true;
 		isOperator        = true;
 		isRightNumber = false;
 		
-		});
+		if(screenHTML == ""){
+			document.querySelector("#screen").innerHTML += "";
+		}else 	if ( isNaN ( Number (last) ) ){
+			document.querySelector("#screen").innerHTML = screenHTML.substr(0, screenHTML.length-1 ) + item.innerHTML;
+		} else {
+			document.querySelector("#screen").innerHTML += item.innerHTML;
+		}
+	});
 });
 
 
 // 3. Clear
 document.querySelector("#end").addEventListener("click",function(){
+	
 	isOperator       =false;
 	isRightNumber= false;
 	isLeftNumber  = false;
+	
 	document.querySelector("#screen").innerHTML = "";
 	document.querySelector("#answer").innerHTML = "";
 });
 document.querySelector("#backspace").addEventListener("click",function(){
 	document.querySelector("#screen").innerHTML = document.querySelector("#screen").innerHTML.substr(0, document.querySelector("#screen").innerHTML.length-1 );
   if(isLeftNumber && isOperator){
+	
 		isRightNumber = false;
+		
 	}else if(isLeftNumber){
+		
 		isOperator = false;
+		
 	}
 	
 });
@@ -89,6 +84,8 @@ function solution( ) {
 	if(isLeftNumber && isOperator && isRightNumber) {
 		document.querySelector("#screen").innerHTML = eval(document.querySelector("#screen").innerHTML.replaceAll("×", "*").replaceAll("÷", "/"));
 		document.querySelector("#answer").innerHTML = "";
+		
 		isSolution = true;
+	
 	}
 }
